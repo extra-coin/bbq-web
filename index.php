cc600c0d30dd76a4b6c9e14c61714ba1468ef558
biou <biou@sous-anneau.org>
2015-06-15 20:47:35 +0200

    logo hack4kids

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 		<a href="http://tcrm-blida.com" title="Tcrm-Blida"><img id="blidalogo" src="img/logoblida.jpg" alt="Blida" /></a>
 		<a href="http://www.cr.rockhal.lu/"><img src="img/logo_rockhal.png" id="rockhallogo" alt="Rockhal Music &amp; Resources" /></a>
 		<a href="http://www.promotechlabs.com/en/partnership/lila-project/"><img id="lilalogo"src="img/LILA_LOGO_NWEIVB_V1.gif" alt="LILA logo"/></a>
-
+		<a href="https://hack4kids.lu/"><img src="img/logo_hack4kids.png" id="hack4kidslogo" alt="hack4kids" /></a>
 	</div>
       </div>	
 
